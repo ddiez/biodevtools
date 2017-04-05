acaa0d7f483bd64382e0e3fba1a2d15fce7d750b
Diego Diez <diego10ruiz@gmail.com>
2017-04-05 16:43:28 +0900

    Initial commit.

---
+++ R/biodev.R
@@ -0,0 +1,15 @@
+#' Base biodev class
+#'
+#' Initializes a common information required for all docker-based biodev tools.
+#'
+biodev <- R6::R6Class("biodev",
+  public = list(
+    outdir = ".",
+    homedir = NULL,
+    voldir = NULL,
+    dockerbin = "docker",
+    initialize = function(dir = getwd()) {
+      self$homedir <- dir
+      self$voldir <- paste0(self$homedir, ":", "/home/biodev")
+    }
+  ))
---
+++ R/package.R
@@ -0,0 +1,9 @@
+#' Support classes to use bioinformatic tools in R using Docker
+#'
+#' Support classes to use bioinformatic tools in R using Docker
+#'
+#' @name biodevtools
+#' @docType package
+#'
+#' @import R6
+NULL
