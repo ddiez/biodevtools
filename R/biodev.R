b1527d2ed8f7481dcbaa1158cba069c0f28393ab
Diego Diez <diego10ruiz@gmail.com>
2017-04-10 17:13:57 +0900

    Check location of docker binary.

--- R/biodev.R
+++ R/biodev.R
@@ -9,7 +9,13 @@ biodev <- R6::R6Class("biodev",
     homedir = NULL,
     voldir = NULL,
     dockerbin = "docker",
-    initialize = function(dir = getwd()) {
+    initialize = function(dir = getwd(), docker.path = NULL) {
+      if (!is.null(docker.path))
+        self$dockerbin = file.path(docker.path, self$dockerbin)
+
+      if (Sys.which(self$dockerbin) == "")
+        stop("the docker binary cannot be found; provide the correct location.")
+
       self$homedir <- dir
       self$voldir <- paste0(self$homedir, ":", "/home/biodev")
     }
